1b378a5b99c7231a33c3918890dd1d27a4b1db03
Honeyv0402 <honeyverma0402@gmail.com>
2026-04-15 21:14:40 +0530

    db changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/project-management");
+        await mongoose.connect("mongodb://mongo:oIFUbWsdRoYaEipGZKgtrpWBBzDlRQol@nozomi.proxy.rlwy.net:41047");
         console.log("MongoDB connected");
     } catch (error) {
         console.error("DB connection failed:", error.message);
